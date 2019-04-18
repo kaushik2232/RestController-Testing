1be88ab7ed53eae4daee8fe5dda76eb332b44a12
kaushik.satya77@gmail.com <kaushik.satya77@gmail.com>
2019-04-18 13:15:12 -0500

    Added JSON Mock TestCase

--- Rest.Testing-Mockito/src/main/java/com/RestController/Testing/Rest/TestingMockito/HelloWorldController.java
+++ Rest.Testing-Mockito/src/main/java/com/RestController/Testing/Rest/TestingMockito/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.RestController.Testing.Rest.TestingMockito;
 
+
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,4 +15,38 @@ public class HelloWorldController {
 		return "Hello World!";
 	}
 
+	@GetMapping(value = "/json", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Hello json() {
+        return new Hello("Greetings", "Hello World");
+    }
+	
+	public static class Hello {
+
+        private String title;
+        private String value;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            this.value = value;
+        }
+
+        public Hello(String title, String value) {
+            this.title = title;
+            this.value = value;
+        }
+
+        public Hello() {
+        }
+    }
 }
--- Rest.Testing-Mockito/src/test/java/com/RestController/Testing/Rest/TestingMockito/HelloWorldControllerTest.java
+++ Rest.Testing-Mockito/src/test/java/com/RestController/Testing/Rest/TestingMockito/HelloWorldControllerTest.java
@@ -3,14 +3,17 @@ package com.RestController.Testing.Rest.TestingMockito;
 import static org.junit.Assert.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -19,9 +22,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 public class HelloWorldControllerTest {
 
 	private MockMvc mockMvc;
-
-//    @Mock
-//    private HelloWorldController helloService;
 
     @InjectMocks
     private HelloWorldController helloResource;
@@ -35,39 +35,21 @@ public class HelloWorldControllerTest {
     @Test
     public void testHelloWorld() throws Exception {
 
-       // when(helloService.hello()).thenReturn("hello");
-
         mockMvc.perform(get("/hello"))
                 .andExpect(status().isOk())
                 .andExpect(content().string("Hello World!"));
 
-        //verify(helloService).hello();
+        
     }
 
-//    @Test
-//    public void testHelloWorldJson() throws Exception {
-//        mockMvc.perform(get("/hello/json")
-//                .accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.title", Matchers.is("Greetings")))
-//                .andExpect(jsonPath("$.value", Matchers.is("Hello World")))
-//                .andExpect(jsonPath("$.*", Matchers.hasSize(2)));
-//    }
-//
-//    @Test
-//    public void testPost() throws Exception {
-//        String json = "{\n" +
-//                "  \"title\": \"Greetings\",\n" +
-//                "  \"value\": \"Hello World\"\n" +
-//                "}";
-//        mockMvc.perform(post("/hello/post")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(json))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.title", Matchers.is("Greetings")))
-//                .andExpect(jsonPath("$.value", Matchers.is("Hello World")))
-//                .andExpect(jsonPath("$.*", Matchers.hasSize(2)));
-//    }
+    @Test
+    public void testHelloWorldJson() throws Exception {
+        mockMvc.perform(get("/hello/json")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.title", Matchers.is("Greetings")))
+                .andExpect(jsonPath("$.value", Matchers.is("Hello World")))
+                .andExpect(jsonPath("$.*", Matchers.hasSize(2)));
+    }
 	
-
 }
