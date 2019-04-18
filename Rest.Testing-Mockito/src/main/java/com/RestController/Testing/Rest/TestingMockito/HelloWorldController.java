9e00182ea8cc03ebdea64d0e02ba44e7fb5a4fe7
kaushik.satya77@gmail.com <kaushik.satya77@gmail.com>
2019-04-18 13:02:39 -0500

    Added MockMVC TestCase

---
+++ Rest.Testing-Mockito/src/main/java/com/RestController/Testing/Rest/TestingMockito/HelloWorldController.java
@@ -0,0 +1,16 @@
+package com.RestController.Testing.Rest.TestingMockito;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloWorldController {
+	
+	@GetMapping
+	public String HelloWorld() {
+		return "Hello World!";
+	}
+
+}
